868e9b087cea43200c6aaf55096de150e482ebc0
Ryan <rmlambert6@gmail.com>
2021-02-02 18:13:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine("Hello " + input + "!");
+            }
+        }
+    }
+}
